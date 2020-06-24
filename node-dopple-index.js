6274ffca467c7d1ceefda2b64b698f7976a93837
Russell R <rdoubleoc@aol.com>
2020-06-24 00:03:55 -0400

    Working POS

--- node-dopple-index.js
+++ node-dopple-index.js
@@ -1,29 +1,34 @@
 const express  = require("express");
-const app = express();
 const ejs = require("ejs");
 const bodyParser = require("body-parser");
 const http = require("http");
 const port = 3000;
+const app = express();
+
 console.log("Starting...");
+
+
 
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.set("view engine", "ejs");
 
-const playerArray = [];
+app.listen(port);
+
+let playerArray = [];
 
 app.get("/", function(req, res){
-	console.log("Serving /");
-    res.render("node-dopple-main", {playerArray: playerArray})
+	console.log("Serving / ...");
+    res.render("node-dopple-main", {playerArray: playerArray});
 })
 
 app.get("/node-dopple-main", function(req, res){
-	console.log("Serving get main");
-    res.render("node-dopple-main", {playerArray: playerArray})
+	console.log("Serving /node-dopple-main ...");
+    res.render("node-dopple-main", {playerArray: playerArray});
 })
 
 app.post("/node-dopple-main", function(req, res){
-	console.log("Serving post");
+	console.log("/node-dopple-main (post) ..");
    let name = req.body.playerName;
    let image = req.body.playerImage;
    let winningPlayer = {name: name, image: image};
