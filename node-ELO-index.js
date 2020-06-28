f7f117d9f76ac8f1c9e497a658c1719e4523e8e7
Russell R <rdoubleoc@aol.com>
2020-06-28 01:04:47 -0400

    Update node-ELO-index.js

--- node-ELO-index.js
+++ node-ELO-index.js
@@ -40,32 +40,48 @@ app.get("/node-dopple-main", function(req, res){
 
 app.post("/node-dopple-main", function(req, res){
 	console.log("Serving /node-dopple-main (post) ..");
+	
 	let name = req.body.playerName;
 	let unserialized = JSON.parse(name);
 	let winner = unserialized[0];
 	let loser = unserialized[1];
+	let winnerLoserArray = {winner: winner, loser: loser};
 	
 	let winnerScoreFile = "Dopples/Actress_Score/" + winner + ".txt";
 	let loserScoreFile = "Dopples/Actress_Score/" + loser + ".txt";
 	
-	let winnerOldScore = fs.readFileSync(winnerScoreFile).toString();
-	let loserOldScore = fs.readFileSync(loserScoreFile).toString();
+	let winnerOldScore = Number(fs.readFileSync(winnerScoreFile));
+	console.log("Winner Old Score:" + winnerOldScore);
+	let loserOldScore = Number(fs.readFileSync(loserScoreFile));
+	console.log("Loser Old Score:" + loserOldScore);
 
 	let winnerELO = ELO(winnerOldScore, loserOldScore);
-	//console.log("Winner ELO Rating: " + winnerELO);
+	console.log("Winner ELO Rating: " + winnerELO);
 	let loserELO = ELO(loserOldScore, winnerOldScore);
-	//console.log("Loser ELO Rating: " + loserELO);
+	console.log("Loser ELO Rating: " + loserELO);
 	
-	let winnerLoserArray = {winner: winner, loser: loser, winnerOldScore: winnerOldScore, loserOldScore: loserOldScore};
+	let k = 32;
+	let winnerNewScore = winnerOldScore + (k * (1 - winnerELO));
+	console.log("Winner New Score: " + winnerNewScore);
+	let loserNewScore = loserOldScore + (k * (0 - loserELO));
+	console.log("Loser New Score: " + loserNewScore);
+	
 	//let image = req.body.playerImage;
+	
+	winnerLoserArray = {winner: winner, loser: loser, winnerOldScore: winnerOldScore, loserOldScore: loserOldScore, winnerNewScore: winnerNewScore, loserNewScore: loserNewScore};
+	
+	console.log(winnerLoserArray);
+	
+	// logArray(winnerLoserArray); // Print Array
+	
 	playerArray.push(winnerLoserArray);
-	logArray(winnerLoserArray); // Print Array
 	console.log("Redirecting to / ...");
 	res.redirect("/");
 });
 
 function ELO(A, B){
-	return (1/(1 + Math.pow(10,((B-A)/400))));
+	let C = B - A;
+	return 1 / (1 + Math.pow(10,(C/400)));
 }
 
 function logArray(theArray){
