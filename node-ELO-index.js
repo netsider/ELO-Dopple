13400905275667ec1a122a6d2d1facf6e476d9f4
Russell R <rdoubleoc@aol.com>
2020-06-28 00:37:28 -0400

    Update node-ELO-index.js

--- node-ELO-index.js
+++ node-ELO-index.js
@@ -6,6 +6,7 @@ const bodyParser = require("body-parser");
 const http = require("http");
 const port = 3000;
 const app = express();
+const fs = require('fs');
 
 console.log("Starting...");
 
@@ -19,10 +20,12 @@ let playerArray = [];
 
 app.get("/", function(req, res){
 	console.log("Serving / ...");
+	
 	if(playerArray.length){
 		console.log(playerArray);
 	}
-    res.render("node-dopple-main", {playerArray: playerArray})
+    
+	res.render("node-dopple-main", {playerArray: playerArray})
 	
 	if(playerArray.length){ // Make sure array empty before user clicks
 		console.log("Resetting playerArray...");
@@ -41,13 +44,29 @@ app.post("/node-dopple-main", function(req, res){
 	let unserialized = JSON.parse(name);
 	let winner = unserialized[0];
 	let loser = unserialized[1];
-	let winnerLoserArray = {winner: winner, loser: loser};
+	
+	let winnerScoreFile = "Dopples/Actress_Score/" + winner + ".txt";
+	let loserScoreFile = "Dopples/Actress_Score/" + loser + ".txt";
+	
+	let winnerOldScore = fs.readFileSync(winnerScoreFile).toString();
+	let loserOldScore = fs.readFileSync(loserScoreFile).toString();
+
+	let winnerELO = ELO(winnerOldScore, loserOldScore);
+	//console.log("Winner ELO Rating: " + winnerELO);
+	let loserELO = ELO(loserOldScore, winnerOldScore);
+	//console.log("Loser ELO Rating: " + loserELO);
+	
+	let winnerLoserArray = {winner: winner, loser: loser, winnerOldScore: winnerOldScore, loserOldScore: loserOldScore};
 	//let image = req.body.playerImage;
 	playerArray.push(winnerLoserArray);
 	logArray(winnerLoserArray); // Print Array
 	console.log("Redirecting to / ...");
 	res.redirect("/");
 });
+
+function ELO(A, B){
+	return (1/(1 + Math.pow(10,((B-A)/400))));
+}
 
 function logArray(theArray){
 	console.log("Logging Array...");
